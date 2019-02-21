16967eedcc7374f16c7f6822ea5b8cb80c28ccf7
munibabu.annachi <munibabu.annachi@accenture.com>
2019-02-21 17:05:06 -0500

    HackathonUI

--- Dockerfile
+++ Dockerfile
@@ -9,3 +9,4 @@ RUN npm run build
 FROM nginx:1.13.12-alpine
 COPY --from=node /usr/src/app/dist/HackathonWeb /usr/share/nginx/html
 COPY ./nginx.conf /etc/nginx/conf.d/default.conf
+EXPOSE 4200
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['../assets/css/style.css']
 })
 export class AppComponent {
-  title = 'HackathonWeb';
+  title = 'Persona Meetup';
 }
