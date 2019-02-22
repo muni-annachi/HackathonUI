bdf7de2fcb599965646f93a561111c7e776912f1
munibabu.annachi <munibabu.annachi@accenture.com>
2019-02-22 13:54:10 -0500

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: 'http://3.16.157.190:3000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiURL: 'http://3.16.157.190:3000'
 };
 
 /*
